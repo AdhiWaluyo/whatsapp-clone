21d9157dea8be4c6b140c50b436f5a274ef49dcc
AdhiWaluyo <adhiw93@gmail.com>
2023-12-04 20:06:47 +0700

    first commit

---
+++ src/firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyBINTBp5WhZjU3iExXfmRgS2gepqGCo2-c",
+	authDomain: "whatsapp-clone-f579b.firebaseapp.com",
+	projectId: "whatsapp-clone-f579b",
+	storageBucket: "whatsapp-clone-f579b.appspot.com",
+	messagingSenderId: "165929209617",
+	appId: "1:165929209617:web:3cb694b107f516f9eb46b0",
+	measurementId: "G-FSD8GVQ0DY"
+};
