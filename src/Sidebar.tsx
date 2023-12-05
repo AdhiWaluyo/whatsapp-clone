f63658917c76732548adfd736c91a1c3e321d30d
AdhiWaluyo <adhiw93@gmail.com>
2023-12-05 21:00:19 +0700

    feat(component): sidebar search

---
+++ src/Sidebar.tsx
@@ -0,0 +1,40 @@
+import "./Sidebar.css";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Avatar, IconButton } from "@mui/material";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+
+      <div className="sidebar__chats">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
